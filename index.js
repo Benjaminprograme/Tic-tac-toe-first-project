0e1b434a6506647f9e0e6b44e14d5db100b81a1d
Njobe <benjo0510@hotmail.com>
2023-09-24 12:30:55 +0200

    gameplay commit f002

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 
+  
 //Gameplay
 //Block Data
-var Table=[];
+var Table=[1,2,3,4,5,6,7,8,9];
+
 
 //Player turns
 var Playing="X"
+//Win draw logic
+function CheckForWin(){
+  
+    if(Table[0]!="1"){
+       if(Table[4]==Table[8]) {
+        Table=[1,2,3,4,5,6,7,8,9];
+        Playing="X"
+       
+       }
+    }
+}
 //Placment
 function SybolPl(id){
-    if(Table[id-1]==null){
+    if(Table[id-1]!=="X" && Table[id-1]!=="O" ){
     var Block=document.getElementById(id);
     Table[id-1]=Playing;
     Block.innerHTML=Playing;
     if (Playing=="X"){
+       
         Playing="O";
     }
     else{
@@ -21,6 +35,13 @@ function SybolPl(id){
   
   
 }
+
 console.log(Table);
+CheckForWin()
 }
+
+
+
+
+
 
