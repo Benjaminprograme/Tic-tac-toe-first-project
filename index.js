db44dfadd2d1dc9e4f32ccdb5f05ac2a6c3bb411
Njobe <benjo0510@hotmail.com>
2023-09-24 19:15:46 +0200

    the buged commit

--- index.js
+++ index.js
@@ -1,61 +1,95 @@
-//Gameplay
-//Block Data
-var Table = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Gameplay
+// Block Data
+var Table = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
 function ClearData() {
-    var Symbols = document.getElementsByClassName("Symbol");
-    for (var i = 0; i < Symbols.length; i++) {
-      if (Symbols[i]) {
-        if(Playing=="X"){
-            alert("The winner is O")
-        }
-        else{
-            alert("The winner is X")
-        }
-        Symbols[i].innerHTML = "";
-        Table = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-        
+  var Symbols = document.getElementsByClassName("Symbol");
+  for (var i = 0; i < Symbols.length; i++) {
+    if (Symbols[i]) {
+      Symbols[i].innerHTML = "";
+      Table = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    }
+  }
+}
+
+// Player turns
+var Playing = "X";
+
+// Win draw logic
+function CheckForWin() {
+  // Other combos
+  if (Table[0] == Table[1] && Table[1] == Table[2]) {
+    ClearData();
+    if (Table[0] == "X") {
+      alert("The winner is X");
+    } else {
+      alert("The winner is O");
+    }
+  }
+  if (Table[0] == Table[3] && Table[3] == Table[6]) {
+    ClearData();
+    if (Table[0] == "X") {
+      alert("The winner is X");
+    } else {
+      alert("The winner is O");
+    }
+  }
+  if (Table[2] == Table[5] && Table[5] == Table[8]) {
+    ClearData();
+    if (Table[2] == "X") {
+      alert("The winner is X");
+    } else {
+      alert("The winner is O");
+    }
+  }
+  if (Table[6] == Table[7] && Table[7] == Table[8]) {
+    ClearData();
+    if (Table[6] == "X") {
+      alert("The winner is X");
+    } else {
+      alert("The winner is O");
+    }
+  }
+
+  // All the combos that contain 4
+  if (Table[4] != null) {
+    if (Table[0] == Table[8] && Table[8] == Table[4]) {
+      ClearData();
+      if (Table[4] == "X") {
+        alert("The winner is X");
+      } else {
+        alert("The winner is O");
+      }
+    }
+    if (Table[1] == Table[7] && Table[7] == Table[4]) {
+      ClearData();
+      if (Table[4] == "X") {
+        alert("The winner is X");
+      } else {
+        alert("The winner is O");
+      }
+    }
+    if (Table[2] == Table[6] && Table[6] == Table[4]) {
+      ClearData();
+      if (Table[4] == "X") {
+        alert("The winner is X");
+      } else {
+        alert("The winner is O");
+      }
+    }
+    if (Table[3] == Table[5] && Table[5] == Table[4]) {
+      ClearData();
+      if (Table[4] == "X") {
+        alert("The winner is X");
+      } else {
+        alert("The winner is O");
       }
     }
   }
-  
+  // Draw
+}
 
-//Player turns
-var Playing = "X";
-//Win draw logic
-function CheckForWin() {
-//Other combos
-if(Table[0]==Table[1] && Table[1]==Table[2]){
-    ClearData()
-}
-if(Table[0]==Table[3] && Table[3]==Table[6]){
-    ClearData()
-}
-if(Table[2]==Table[5] && Table[5]==Table[8]){
-    ClearData()
-}
-if(Table[6]==Table[7] && Table[7]==Table[8]){
-    ClearData()
-}
-    //All the combos that contain 4
-if(Table[4]!=null){
-        if(Table[0]==Table[8] && Table[8]==Table[4]){
-            ClearData()
-        }
-        if(Table[1]==Table[7] && Table[7]==Table[4]){
-            ClearData()
-        }
-        if(Table[2]==Table[6] && Table[6]==Table[4]){
-            ClearData()
-        }
-        if(Table[3]==Table[5] && Table[5]==Table[4]){
-            ClearData()
-        }
-    
-    }
-    //Draw
-    
-}
-//Game
+// Game
 function SybolPl(id) {
   if (Table[id - 1] !== "X" && Table[id - 1] !== "O") {
     var Block = document.getElementById(id);
@@ -67,6 +101,6 @@ function SybolPl(id) {
       Playing = "X";
     }
   }
-  console.log(Table); //Testing
+  console.log(Table); // Testing
   CheckForWin();
 }
