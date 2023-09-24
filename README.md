3341de9860a53f4cc3e6551ba2ac346790ec4050
Njobe <benjo0510@hotmail.com>
2023-09-24 11:14:44 +0200

    gameplay logic f001

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Js-learning
-Learning js is a project folder where I will put up my exercises
+Learning js is a project folder where I will put up my exercises.
--- index.js
+++ index.js
@@ -1,4 +1,26 @@
+
+//Gameplay
+//Block Data
+var Table=[];
+
+//Player turns
+var Playing="X"
+//Placment
 function SybolPl(id){
+    if(Table[id-1]==null){
     var Block=document.getElementById(id);
-    
+    Table[id-1]=Playing;
+    Block.innerHTML=Playing;
+    if (Playing=="X"){
+        Playing="O";
+    }
+    else{
+        Playing="X";
+    }
+ 
+  
+  
 }
+console.log(Table);
+}
+
