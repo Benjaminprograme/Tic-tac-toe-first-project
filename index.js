ae9413b55272beea3a07a64807edc125be278036
Njobe <benjo0510@hotmail.com>
2023-09-25 17:13:50 +0200

    finish game commit

--- index.js
+++ index.js
@@ -1,106 +1,80 @@
-// Gameplay
-// Block Data
-var Table = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+function Start(){
+    document.getElementById("Board").style.display="flex"
+    var el=document.getElementsByTagName("input");
+    el.setAttribute("readonly",true)
+}
 
+//Gameplay
+//Block Data
+var Table = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 function ClearData() {
-  var Symbols = document.getElementsByClassName("Symbol");
-  for (var i = 0; i < Symbols.length; i++) {
-    if (Symbols[i]) {
-      Symbols[i].innerHTML = "";
-      Table = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var Symbols = document.getElementsByClassName("Symbol");
+    for (var i = 0; i < Symbols.length; i++) {
+      if (Symbols[i]) {
+       Table = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        Symbols[i].innerHTML = "";
+    }
     }
   }
-}
+  
 
-// Player turns
+//Player turns
 var Playing = "X";
-
-// Win draw logic
+//Win draw logic
 function CheckForWin() {
-  // Other combos
-  if (Table[0] == Table[1] && Table[1] == Table[2]) {
-    ClearData();
-    if (Table[0] == "X") {
-      alert("The winner is X");
-    } else {
-      alert("The winner is O");
-    }
+//Other combos
+if(Table[0]==Table[1] && Table[1]==Table[2]){
+    alert("The winner is "+ Playing)
+    ClearData()
   }
-  if (Table[0] == Table[3] && Table[3] == Table[6]) {
-    ClearData();
-    if (Table[0] == "X") {
-      alert("The winner is X");
-    } else {
-      alert("The winner is O");
-    }
-  }
-  if (Table[2] == Table[5] && Table[5] == Table[8]) {
-    ClearData();
-    if (Table[2] == "X") {
-      alert("The winner is X");
-    } else {
-      alert("The winner is O");
-    }
-  }
-  if (Table[6] == Table[7] && Table[7] == Table[8]) {
-    ClearData();
-    if (Table[6] == "X") {
-      alert("The winner is X");
-    } else {
-      alert("The winner is O");
-    }
-  }
-
-  // All the combos that contain 4
-  if (Table[4] != null) {
-    if (Table[0] == Table[8] && Table[8] == Table[4]) {
-      ClearData();
-      if (Table[4] == "X") {
-        alert("The winner is X");
-      } else {
-        alert("The winner is O");
-      }
-    }
-    if (Table[1] == Table[7] && Table[7] == Table[4]) {
-      ClearData();
-      if (Table[4] == "X") {
-        alert("The winner is X");
-      } else {
-        alert("The winner is O");
-      }
-    }
-    if (Table[2] == Table[6] && Table[6] == Table[4]) {
-      ClearData();
-      if (Table[4] == "X") {
-        alert("The winner is X");
-      } else {
-        alert("The winner is O");
-      }
-    }
-    if (Table[3] == Table[5] && Table[5] == Table[4]) {
-      ClearData();
-      if (Table[4] == "X") {
-        alert("The winner is X");
-      } else {
-        alert("The winner is O");
-      }
-    }
-  }
-  // Draw
+if(Table[0]==Table[3] && Table[3]==Table[6]){
+    alert("The winner is "+ Playing)
+    ClearData()
 }
-
-// Game
+if(Table[2]==Table[5] && Table[5]==Table[8]){
+    alert("The winner is "+ Playing)
+    ClearData()
+}
+if(Table[6]==Table[7] && Table[7]==Table[8]){
+    alert("The winner is "+ Playing)
+    ClearData()
+}
+    //All the combos that contain 4
+if(Table[4]!=null){
+        if(Table[0]==Table[8] && Table[8]==Table[4]){
+            alert("The winner is "+ Playing)
+            ClearData()
+        }
+        if(Table[1]==Table[7] && Table[7]==Table[4]){
+            alert("The winner is "+ Playing)
+            ClearData()
+        }
+        if(Table[2]==Table[6] && Table[6]==Table[4]){
+            alert("The winner is "+ Playing)
+            ClearData()
+        }
+        if(Table[3]==Table[5] && Table[5]==Table[4]){
+            alert("The winner is "+ Playing)
+            ClearData()
+        }
+    
+    }
+    //Draw
+    
+}
+//Game
 function SybolPl(id) {
   if (Table[id - 1] !== "X" && Table[id - 1] !== "O") {
     var Block = document.getElementById(id);
-    Table[id - 1] = Playing;
     Block.innerHTML = Playing;
+    Table[id - 1] = Playing;
+    CheckForWin();
     if (Playing == "X") {
       Playing = "O";
     } else {
       Playing = "X";
     }
   }
-  console.log(Table); // Testing
-  CheckForWin();
+  console.log(Table); //Testing
 }
+
